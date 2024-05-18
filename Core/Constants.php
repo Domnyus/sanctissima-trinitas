8f330f32b4da38403f83e3a59df2d273ffed506d
Domnyus <127503710+Domnyus@users.noreply.github.com>
2024-05-18 15:51:47 -0300

    Update Constants.php

--- Core/Constants.php
+++ Core/Constants.php
@@ -6,7 +6,7 @@ class Constants
     public CONST BAD_REQUEST = 400;
     public CONST INTERNAL_SERVER_ERROR = 500;
     public CONST DBHOST = "localhost:3306";
-    public CONST DBNAME = "exemplo";
+    public CONST DBNAME = "db";
     public CONST DBUSER = "root";
     public CONST DBPASS = "root";
     public CONST VIEWS = __DIR__ . "/Views/";
